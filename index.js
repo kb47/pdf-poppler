b275c28c64a6606bb09175428048bdc014edfcde
Khishigbaatar <khishigbaatar@2xcoding.com>
2018-07-04 13:31:32 +0800

    Update to poppler 0.66 for mac

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ else if (platform === 'darwin') {
         __dirname,
         'lib',
         'osx',
-        'poppler-0.62',
+        'poppler-0.66',
         'bin'
     );
 
@@ -41,7 +41,7 @@ else if (platform === 'darwin') {
         __dirname,
         'lib',
         'osx',
-        'poppler-0.62',
+        'poppler-0.66',
         'lib'
     );
 
@@ -60,9 +60,9 @@ else if (platform === 'darwin') {
     spawn('chmod', ['-R', '755', `${libRoot}`]);
 
     // change name for every executables
-    spawn('install_name_tool', ['-change', `/usr/local/Cellar/poppler/0.62.0/lib/libpoppler.73.dylib`, `${path.join(dyldPath, 'libpoppler.73.0.0.dylib')}`, `${path.join(popplerPath, 'pdfinfo')}`]);
-    spawn('install_name_tool', ['-change', `/usr/local/Cellar/poppler/0.62.0/lib/libpoppler.73.dylib`, `${path.join(dyldPath, 'libpoppler.73.0.0.dylib')}`, `${path.join(popplerPath, 'pdftocairo')}`]);
-    spawn('install_name_tool', ['-change', `/usr/local/Cellar/poppler/0.62.0/lib/libpoppler.73.dylib`, `${path.join(dyldPath, 'libpoppler.73.0.0.dylib')}`, `${path.join(popplerPath, 'pdfimages')}`]);
+    spawn('install_name_tool', ['-change', `/usr/local/Cellar/poppler/0.66.0/lib/libpoppler.77.dylib`, `${path.join(dyldPath, 'libpoppler.77.0.0.dylib')}`, `${path.join(popplerPath, 'pdfinfo')}`]);
+    spawn('install_name_tool', ['-change', `/usr/local/Cellar/poppler/0.66.0/lib/libpoppler.77.dylib`, `${path.join(dyldPath, 'libpoppler.77.0.0.dylib')}`, `${path.join(popplerPath, 'pdftocairo')}`]);
+    spawn('install_name_tool', ['-change', `/usr/local/Cellar/poppler/0.66.0/lib/libpoppler.77.dylib`, `${path.join(dyldPath, 'libpoppler.77.0.0.dylib')}`, `${path.join(popplerPath, 'pdfimages')}`]);
 }
 else {
     console.error(`${platform} is NOT supported.`);
